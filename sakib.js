72265c8bfebfbc98d4c16180b59ad64ad5f5882d
SakibFakir69 <FakirSakib22232@gmail.com>
2024-06-28 15:27:11 +0600

    scrool

--- sakib.js
+++ sakib.js
@@ -1,1 +1,35 @@
-console.log("sakib");
+
+
+
+
+
+const scrool= () =>{
+    window.scrollTo(0,0);
+}
+let button=document.getElementById("button");
+button.addEventListener("click",()=>{
+    window.scrollBy(window.innerHeight,window.innerHeight);
+    console.log(window.innerHeight)
+
+
+
+    console.log("ok");
+})
+
+let box=document.getElementById("box");
+
+if(window.innerHeight==window.innerHeight){
+
+    let div=document.createElement("div");
+    let button=document.createElement("button");
+    button.innerText="click me";
+    div.appendChild(button);
+    box.appendChild(div);
+
+    button.addEventListener("click",()=>{
+        window.scrollTo(0,0);
+    })
+
+}else{
+    alert("not founded");
+}
