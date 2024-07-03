6093527da82506cac7e538ffcf1b5c487d0fbcc9
SakibFakir69 <FakirSakib22232@gmail.com>
2024-07-03 13:29:41 +0600

    text add

--- sakib.js
+++ sakib.js
@@ -3,16 +3,24 @@
 
 
 
+
 let button=document.getElementById("button");
-let buttonto=document.getElementById("buttonto");
+let input=document.getElementById("input");
+let box=document.getElementById("box")
 
-
+let text=0;
 button.addEventListener("click",()=>{
-    alert("Welcome my new web page");
+    if(input.value== " "){
+        alert("Enter your value")
+    }
+    text++;
+    let div=document.createElement("div");
+    let li=document.createElement("li");
+    li.style.fontSize="1.2em";
+    li.style.color="green";
+    li.style.fontWeight="blod";
+    div.appendChild(li);
+    box.appendChild(div);
+    li.innerText=`${text} `+input.value;
+    input.value= " ";
 })
-
-buttonto.addEventListener("click",()=>{
-    prompt("Enter your name");
-})
-
-
